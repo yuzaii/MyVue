4eecfb8ef7592dadf10b138a1e4b84ccdc893b1c
yuzai_win <yuzaii@qq.com>
2022-12-03 22:39:43 +0800

    事件

--- js/vue.js
+++ js/vue.js
@@ -395,12 +395,14 @@
     /**
      * Show production mode tip message on boot?
      */
-    productionTip: "development" !== 'production',
+    // productionTip: "development" !== 'production',
+    productionTip: false,
 
     /**
      * Whether to enable devtools
      */
-    devtools: "development" !== 'production',
+    // devtools: "development" !== 'production',
+    devtools: false,
 
     /**
      * Whether to record perf
