7dd13811ddce2a126b3023a91d25026461e88a24
zhouliangyu <yuzaii@qq.com>
2022-12-15 16:34:25 +0800

    单文件组件

---
+++ 尚硅谷/19_单文件组件/Student.vue
@@ -0,0 +1,34 @@
+<template>
+  <!--组建的结构-->
+  <div class="demo">
+    <h2>学校名称：{{ schoolname }}</h2>
+    <h2>学校地址：{{ schooladr }}</h2>
+    <button @click="showname">点我提示学校名</button>
+  </div>
+</template>
+
+<script>
+// 组件交互的相关代码
+//创建school组件
+export default Vue.extend({
+  name: 'School',
+  // el: "#app", // 不能写el配置项 vm配置
+  data() {
+    return {
+      schoolname: "北京大学",
+      schooladr: "北京"
+    }
+  },methods: {
+    showname(){
+      alert(this.schoolname)
+    }
+  },
+})
+// export default school
+</script>
+<style>
+/*组件的样式*/
+.demo{
+  background-color: aquamarine;
+}
+</style>
